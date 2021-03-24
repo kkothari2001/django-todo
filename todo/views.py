72337e6cf7da3c1d204006825e9b5dbd545d88f2
Kush Kothari <kushkothari2001@gmail.com>
2021-03-25 03:33:57 +0530

    Added functionality to add new items to the list

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Todo
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    todo_list = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title=request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html', {"todo_list": todo_list})
